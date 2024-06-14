07e21d1175d0652af2951db59a66b5ead18066ab
unknown <tranbaohoang70@gmail.com>
2024-06-14 12:52:17 +0700

    clone1

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:main_project/screen/home_screen.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
---
+++ lib/widgets/product_card.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ProductCard extends StatelessWidget{
+  String img;
+  ProductCard(this.img, {super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return Container();
+  }
+}
